e5339b2ebdd5347e366f67c2cf8299cbfec88365
patdz <patdz@ymipro.com>
2022-11-27 13:47:19 +0800

    按需引入 element+
    npm install element-plus --save
    npm install -D unplugin-vue-components unplugin-auto-import

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <el-button type="danger">I am ElButton</el-button>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
